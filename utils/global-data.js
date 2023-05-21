ae0a61bc0c7b96faf4c4c3d147122d158e005f45
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-05-21 20:48:34 +0900

    Update global-data.js
    data update

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : "MicKoreaYoutube's nextjs-test";
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Next.js Blog Theme';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'ⓒ 2023. MicKoreaYoutube all rights reserved.';
 
   return {
     name,
